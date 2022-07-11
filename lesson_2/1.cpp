0637983515944ca842997b0d37bbb41dcbf7f65d
= <metrixim@yandex.ru>
2022-07-11 15:48:26 +0300

    class p

---
+++ lesson_2/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Person{
+    string name;
+    int age;
+    string sex;
+    int weight;
+};
+
+
+
+
+int main(){
+    Person p;
+}
